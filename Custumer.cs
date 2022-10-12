f01f9a3c857aabc4c14c6b6e2a103d81d2b8b88b
Hauvgaard <math214x@mail.hansenberg.dk>
2022-10-12 14:58:15 +0200

    Customer + MainStory

---
+++ Custumer.cs
@@ -0,0 +1,14 @@
+class customer{
+    int money;
+    string job;
+    string Gender;
+    string name;
+    string prefix; //mr. ms. etc
+
+    public customer(int money, string jobs, string gender, string name){
+        //money, job, gender random
+        console.writeline("What is your name?");
+        name = console.readLine();
+    }
+
+}
