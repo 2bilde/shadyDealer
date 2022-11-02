c57af05896aa0e7f85b5aaa66fe6e5714153febd
Hauvgaard <math214x@mail.hansenberg.dk>
2022-11-02 09:31:23 +0100

    CUstomer improved

--- Customer.cs
+++ Customer.cs
@@ -8,10 +8,20 @@ class customer{
 
     public customer(int money, string jobs, string gender, string name){
         //money, job, gender random
-        console.writeline("What is your full name?");
-        name = console.readLine();
-        surName = name.Split(' ');
-        consoæe.writeline("what is your title (Mr., Ms., Mrs. etc");
+        Console.WriteLine("What is your full name?");
+        name = Console.ReadLine();
+        while (name == null){
+            Console.WriteLine("please try again");
+            name = Console.ReadLine();
+        }
+        string[] splitted = name.Split(' ');
+        surName = splitted[splitted.Length-1];
+        Console.WriteLine("what is your title (Mr., Ms., Mrs. etc");
+        title = Console.ReadLine();
+        while (title == null){
+            Console.WriteLine("please try again");
+            title = Console.ReadLine();
+        }
+        Console.WriteLine("Hello " + title + ' ' + surName + ". So nice to see you today. It is my understanding that your budget is $" + budget);
     }
-
 }
