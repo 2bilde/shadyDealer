7ac509a4b2b7da3b2fb6d50b5b413d21027781ab
Tobias Christiansen <tobi499a@mail.hansenberg.dk>
2022-12-08 09:14:43 +0100

    c = new costumer
    idk

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Car car1 = new Car(0, 0, 0, 0, 0);
-
+customer c = new customer;
 Console.ReadLine();
