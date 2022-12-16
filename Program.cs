4b205a4937e6c2a10c6016628a03218a891ea39d
Tobias Christiansen <tobi499a@mail.hansenberg.dk>
2022-12-16 14:56:23 +0100

    Masse fix

--- Program.cs
+++ Program.cs
@@ -1,9 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-customer c = new customer(1, "placeholderName", "placeholdBudget");
-
-Console.ReadLine();
-
-
-
+customer c = new customer();
--- Salesman.cs
+++ Salesman.cs
@@ -1,13 +1,24 @@
 public class salesman
 {
-    public int price = 0;
+    customer c;
 
-
-
-    public salesman(int price,customer.budget)
+    public salesman(customer c)
     {
-    
+        this.c = c;
     }
+
+    public void buy(Car b)
+    {
+        Console.WriteLine("Here you go " + c.title);
+
+        c.budget = c.budget - b.price;
+
+        Console.WriteLine("Your new budget is");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(c.budget);
+
+    }
+    
 
 
 }
