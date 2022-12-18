fe0c38afe012ef1d6e0ae87b91ee15dabeac439e
Hauvgaard <math214x@mail.hansenberg.dk>
2022-12-18 21:47:56 +0100

    ToDoliste i "program"
    lavede en todo liste i toppen af "program" filen

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿
+﻿// ting der skal laves:
+//  -   secret code i basement
+//  -   vise budget før og efter køb + bedre/mere flydende historie
+//  -   når man enter basement og går ud fortsætter man med bilen. evt ændres?
+//  -   rent faktisk lave forskellige biler med forskellige hjul og topfart (evt gøre prisen afhængig af topfard og døre osv)
 
 Console.WriteLine("What is your full name?");
 
