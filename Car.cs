ce6b4e52981e1c38d00e85bfdbb75f921a7ff7a5
Hauvgaard <math214x@mail.hansenberg.dk>
2022-12-30 19:46:12 +0100

    lille fix

--- Car.cs
+++ Car.cs
@@ -59,53 +59,6 @@ public class Car
             {
                 Console.WriteLine("Well here is another model");
             }
-            /*switch (model)
-            {
-                case CarType.Tesla: 
-                    Console.WriteLine("\nAhh yes the " + model.Name() + " model. with " + door + "doors and a top speed of " + speed);
-
-                    Console.WriteLine("\nThe cars price is $" + price);
-
-                    if (price >= c.budget)
-                        Console.WriteLine("You have enough money to buy the vehicle");
-                    Console.WriteLine("Would you like to buy it?");
-                    answer = GetStringInput(c);
-                    while (answer != "yes" || answer != "no")
-                    {
-                        Console.WriteLine("Im sorry i didnt understand that. Please answer \nyes \nno");
-                        answer = GetStringInput(c);
-                    }
-                    if (answer == "yes")
-                    {
-                        new salesman(c);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Well here is another model");
-                    }
-                    break;
-
-                case CarType.Mercedes: 
-                    Console.WriteLine("\nAhh yes the " + model.Name() + " model. with " + door + "doors and a top speed of " + speed);
-                    break;
-
-                case CarType.BMW:
-                    Console.WriteLine("\nAhh yes the " + model.Name() + " model. with " + door + "doors and a top speed of " + speed);
-                    break;
-
-                case CarType.Toyota:
-                    Console.WriteLine("\nAhh yes the " + model.Name() + " model. with " + door + "doors and a top speed of " + speed);
-                    break;
-
-                case CarType.Citröen:
-                    Console.WriteLine("\nAhh yes the " + model.Name() + " model. with " + door + "doors and a top speed of " + speed);
-                    break;
-
-                case CarType.Aston_Martin:
-                    Console.WriteLine("\nAhh yes the " + model.Name() + " model. with " + door + "doors and a top speed of " + speed);
-                    break;
-            }*/
-
         }
         Console.WriteLine("Unfortunetly we don't have any more cars. So get the fuck out of here!");
         GetStringInput(customer);
